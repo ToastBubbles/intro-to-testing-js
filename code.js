9d0c295278801f4b0441e638d78f147b3f505dac
Jeffrey Neal <jeffneal11@gmail.com>
2022-11-29 13:47:47 -0600

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+
+function sayHello(name){
+
+    if(name) {
+        return "Hello, " + name.toString()
+    }
+    else{
+        return "Hello, "
+    }
+
+}
